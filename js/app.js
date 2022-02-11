85b6c4ba389c11bc343eaf8b39e56ffd6cb6f89c
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-11 14:34:07 -0500

    Add html and js structor for dice and roll button

--- js/app.js
+++ js/app.js
@@ -7,11 +7,17 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const dice = document.querySelectorAll('div')
+const rollBtn = document.querySelector('button')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+rollBtn.addEventListener('click', handleClick)
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function handleClick(evt) {
+  let diceId = parseInt(evt.target.id)
+  diceId.innerText = Math.floor(Math.random() * 5);
+}
