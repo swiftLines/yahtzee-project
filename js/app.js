b5a8887b35b5ca1c74867318ab3264fc2952c01e
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-11 14:56:50 -0500

    Generate random number values for dice when roll button is clicked

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-const dice = document.querySelectorAll('div')
+const diceX = document.querySelectorAll('div')
 const rollBtn = document.querySelector('button')
 
 
@@ -18,6 +18,9 @@ rollBtn.addEventListener('click', handleClick)
 /*-------------------------------- Functions --------------------------------*/
 
 function handleClick(evt) {
-  let diceId = parseInt(evt.target.id)
-  diceId.innerText = Math.floor(Math.random() * 5);
+  // let diceId = parseInt(evt.target.id)
+  diceX.forEach((dice) => {
+    dice.innerText = Math.floor(Math.random() * 5) + 1
+  })
+
 }
