151754ad4d25258a4388e20e7369a64a40a08229
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-11 16:01:57 -0500

    Add a p element that displays total dice value

--- js/app.js
+++ js/app.js
@@ -3,24 +3,36 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+const rollTotal = []
 
 
 /*------------------------ Cached Element References ------------------------*/
 const diceX = document.querySelectorAll('div')
 const rollBtn = document.querySelector('button')
-
+let diceTotal = document.querySelector('#total')
 
 /*----------------------------- Event Listeners -----------------------------*/
-rollBtn.addEventListener('click', handleClick)
+rollBtn.addEventListener('click', diceRoll)
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-function handleClick(evt) {
+function init() {
+  // for (let i = 0; i < 5; i++) {
+  //   rollTotal[i] = null;
+  // }
+}
+
+function diceRoll(evt) {
   // let diceId = parseInt(evt.target.id)
   diceX.forEach((dice) => {
-    dice.innerText = Math.floor(Math.random() * 5) + 1
+    let diceValue = Math.floor(Math.random() * 5) + 1
+    dice.innerText = diceValue;
+    rollTotal.push(diceValue)
   })
-
+  
+  let total = rollTotal.reduce((sum, cur) => {
+    return sum + cur
+  }, 0)
+  diceTotal.innerText = total;
 }
