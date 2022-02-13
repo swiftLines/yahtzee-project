2d70d7152530ade004a180e844722806285f877d
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-13 16:01:21 -0500

    Add code to change which players turn it is and to clear roll

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let rollTotal=[], selectTotal=[], rollCount, picks=[]
-
+let rollTotal=[], selectTotal=[], rollCount, picks=[], turn, pickCount
+//may not need rollTotal as global variable
 
 /*------------------------ Cached Element References ------------------------*/
 const diceX = document.querySelectorAll('div')
@@ -11,6 +11,7 @@ const rollBtn = document.querySelector('button')
 let diceTotal = document.querySelector('#total')
 let chooseDice = document.querySelector('#select')
 let choices = document.querySelector('#choices')
+let messageEl = document.querySelector('#message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 rollBtn.addEventListener('click', diceRoll)
@@ -26,70 +27,86 @@ init()
 
 //initialize state variables
 function init() {
-  //initialize array to store dice values player that player chooses for single turn
+  //initialize array to store dice values player that player chooses for 
+  //single turn
   for (let i = 0; i < 5; i++) {
     rollTotal[i] = null
   }
 
-  // d
-
   //initialize role counter
   rollCount = 0
+
+  //initialize whose turn it is to 1 (player 'one')
+  turn = 1
+
+  //initialize counter for number of dice selected
+  pickCount = 1
+
+  render()
 }
 
 function render() {
 
+//Render a message reflecting the current game state:
+  let message
+
+  if (rollCount === 3) {
+    turn *= -1
+    rollCount = 0
+  }
+
+  if (turn === 1) {
+    message = `player ones roll`
+  } else {
+    message = 'player twos roll'
+  }
+  
+  messageEl.innerText = `${message}`
+
+  //***** Maybe render if (picks.length > 0) instead of handling in diceRoll
+
 }
 
 //rolls dice
-function diceRoll(evt) { //evt needed??
+function diceRoll(evt) { //REMOVE evt if dont use 
+
   let diceValue
-  let iteration = 1
+//***** NEED TO handle end turn events in this function or call to an endTurn() 
   if (rollCount === 3) {
-    // iteration = 0
-    return
+    rollTotal = []
+    
+    render()
   } else {
-    //XXXXXXX KEEP DICE REMOVED from play not just after a single roll
-    // diceX.slice(0, size)
+    //Keep selected dice removed from play 
+    //***** STILL need to have correct number of dice removed for third roll...
+    // MAYBE add to render... OR put with 
     if (picks.length > 0) {
       for (let dice of diceX) {
-        if (iteration === picks.length) {
+        if (pickCount === picks.length) {
           dice.innerText = ''
-          console.log('hi')
         } else {
           diceValue = Math.floor(Math.random() * 5) + 1
           dice.innerText = diceValue;
-          iteration++
+          pickCount++
         }
       }
     } else {
       diceX.forEach((dice) => {
         diceValue = Math.floor(Math.random() * 5) + 1
         dice.innerText = diceValue;
-      //could just replace some code in here with reduce or another built in method for dry code
+      //maybe could just replace some code in here with reduce or another built in
+      // method for dry code
         rollTotal.push(diceValue) 
         
       })
     }
-    // }
-  // if (picks.length > 0) {
-  //   for (let i = 0; i < 5 - picks.length; i++) {
-  //     diceValue = Math.floor(Math.random() * 5) + 1
-  //   }
-  // }
-      
-    }
-
-    // if (picks.length > 0) {
-    //   //remove dice element
-    //   for(let i = 1 d)
-    //   dice.remove()
-    // }
+  }
 
   //track roll amount
   rollCount++
   
-  //***NEED to total all dice values for only a single roll
+  //***NEED to total all dice values for only a single roll 
+  //(maybe do towards qualifying combos later....)
   let total = rollTotal.reduce((sum, cur) => {
     return sum + cur
   }, 0)
@@ -102,12 +119,15 @@ function diceRoll(evt) { //evt needed??
 
 //allow player to select dice values they want to keep for their combo
 function selectDice(evt) { 
-  //NEED to be able to click under Your Picks to be able to reroll a dice/ put back / allow access to with diceX 
+  //NEED to be able to click under Your Picks to be able to reroll a dice/ 
+  //put back / allow access to with diceX 
 
   //if evt.target.id has been selected previously MAYBE.....
   let choice = parseInt(evt.target.innerText)
   picks.push(choice)
   //have choices display all at once
+  //***** Will need to place in html elements so that you can click to place 
+  //back into dice cup
   choices.innerText = picks
 
   //sum up value of dice selected
@@ -126,9 +146,11 @@ function selectDice(evt) {
 }
 
 function putDiceBackInPlay() {
-//NEED to be able to click under Your Picks to be able to reroll a dice/ put back / allow access to with diceX 
+//NEED to be able to click under Your Picks to be able to reroll a dice/ 
+//put back / allow access to with diceX 
 
-// ????? THIS MAY BE WHEN I NEED TO START CREATING THE PLAYER OBJECT BECAUSE OF EVENT HANDLING ??????
+// ????? THIS MAY BE WHEN I NEED TO START CREATING THE PLAYER OBJECT 
+//BECAUSE OF EVENT HANDLING ??????
 
 //MAYBE NOT YET... MAKE CHOICES A LIST IN HTML and CAN addEventListener
 }
