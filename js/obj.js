58a9a5b3142bb618c9b201aa9acb771c29995102
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-13 17:18:09 -0500

    Add initial player one object

---
+++ js/obj.js
@@ -0,0 +1,28 @@
+const playerOne = {
+  //maybe also have the round count in here so if player has done 
+  //13 rounds they cannot roll anymore...or maybe not
+  
+  totalScore: null, //or maybe an array
+  //upper
+  aces: null,
+  twos: null,
+  threes: null,
+  fours: null,
+  fives: null,
+  sixes: null,
+  initialUpperTotal: null,
+  bonus: null,
+  upperTotal: null,
+  //lower
+  threeOfAKind: null,
+  fourOfAKind: null,
+  fullHouse: null,
+  lowStraight: null,
+  highStraight: null,
+  yahtzee: null,
+  chance: null,
+  yahtzeeBonus: null,
+  lowerTotal: null,
+  grandTotal: null,
+
+}
