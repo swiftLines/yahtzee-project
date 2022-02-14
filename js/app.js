1e0522c1a2ba72b85e2bc31090dd6c33765d95ec
Jeremy Underwood <underwood.j248@gmail.com>
2022-02-14 15:35:11 -0500

    Add initial function to handle end of turn and establish pseudocode for next steps

--- js/app.js
+++ js/app.js
@@ -79,6 +79,8 @@ function diceRoll(evt) { //REMOVE evt if dont use
   let diceValue
 //***** NEED TO handle end turn events in this function or call to an endTurn() 
   if (rollCount === 3) {
+    //endTurn()
+    //Maybe put below statements into endTurn()
     rollTotal = []
     picks = []
     choices.innerText = ''
@@ -86,8 +88,6 @@ function diceRoll(evt) { //REMOVE evt if dont use
     render()
   } else {
     //Keep selected dice removed from play 
-    //***** STILL need to have correct number of dice removed for third roll...
-    // MAYBE add to render... OR put with 
     if (picks.length > 0) {
       for (let dice of diceX) {
         if (pickCount === picks.length) {
@@ -104,8 +104,7 @@ function diceRoll(evt) { //REMOVE evt if dont use
       diceX.forEach((dice) => {
         diceValue = Math.floor(Math.random() * 6) + 1
         dice.innerText = diceValue;
-      //maybe could just replace some code in here with reduce or another built in
-      // method for dry code
+      // maybe use buit-in method for dry code
         rollTotal.push(diceValue) 
         
       })
@@ -129,15 +128,10 @@ function diceRoll(evt) { //REMOVE evt if dont use
 
 //allow player to select dice values they want to keep for their combo
 function selectDice(evt) { 
-  //NEED to be able to click under Your Picks to be able to reroll a dice/ 
-  //put back / allow access to with diceX 
-  //if evt.target.id has been selected previously MAYBE.....
   let choice = parseInt(evt.target.innerText)
   picks.push(choice)
   //have choices display all at once
-// ******* CAN REMOVE BELOW AFTER code to fill html elements below
-  //choices.innerText = picks
-  console.log(picks)
+
   //sum up value of dice selected
   let total = picks.reduce((sum, cur) => {
     return sum + cur
@@ -147,7 +141,7 @@ function selectDice(evt) {
   //Remove selected dice from play
   evt.target.innerText = ''
 
-//***** Will need to place in html elements so that you can click to place 
+//place in html elements so that you can click to place 
 //back into dice cup
    let newDiv = document.createElement('div')
    newDiv.addEventListener('click', putDiceBackInPlay)
@@ -182,4 +176,20 @@ function putDiceBackInPlay(evt) {
   //choices.innerText = picks
   evt.target.innerText = ''
   pickCount--
+}
+
+function endTurn() {
+  // 1) need to push any additional dice on board to picks[]
+  // 2) compare picks[] (or may need a new data structure) to categories so that player can be aware of 
+  //    categories qualified for
+  // 3) allow player to apply their picks to a category 
+  // 4) save category score in player object
+  // 5) clear appropriate variables and data structures to prepare for next turn/round
+
+  //MAYBE handle some of the below statements in init()
+  // rollTotal = []
+  // picks = []
+  // choices.innerText = ''
+  // chooseDice.innerText = ''
+  // render()
 }
